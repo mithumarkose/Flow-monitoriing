723d93f8213d480fc3aab51182df3311de23d774
Mithu Markose <mithumarkose@users.noreply.github.com>
2018-04-09 22:58:38 +0530

    Add files via upload

---
+++ flowmeter_arduino.ino
@@ -0,0 +1,40 @@
+byte statusled    = 13;
+byte sensorInterrupt = 0;
+byte sensorPin       = 2;
+// The hall-effect flow sensor outputs approximately 4.5 pulses per second per
+// litre/minute of flow.
+float calibrationFactor = 4.5;
+volatile byte pulseCount;
+float flowRate;
+unsigned long oldTime;
+
+void pulseCounter()
+{
+  pulseCount++;
+}
+void setup()
+{
+  Serial.begin(38400);
+  pinMode(statusLed, OUTPUT);
+  digitalWrite(statusLed, HIGH);
+  pinMode(sensorPin, INPUT);
+  digitalWrite(sensorPin, HIGH);
+  pulseCount        = 0;
+  flowRate          = 0.0;
+  oldTime           = 0;
+  attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+}
+void loop()
+{
+  if ((millis() - oldTime) > 1000)
+  {
+    detachInterrupt(sensorInterrupt);
+    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
+    unsigned int frac;
+    // Print the flow rate for this second in litres / minute
+    Serial.println(int(flowRate));
+    pulseCount = 0;
+    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+  }
+}
+
